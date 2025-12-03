696f91511dfec248d6e1603a68e03eb43ffcb94a
Zekeriyaev Danil <zekeriyaev_d@auca.kg>
2025-12-03 21:15:20 +0600

    adding BEM metology css

--- src/components/ColorPicker.jsx
+++ src/components/ColorPicker.jsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from 'react'
-
 import './ColorPicker.css'
 
 export default function ColorPicker() {
@@ -10,16 +9,34 @@ export default function ColorPicker() {
     }, [color])
 
     return (
-        <div className="App" style={{
-            background: color,
-            color: 'white'
-          }}>
-            <h2>Count: 12</h2>
-            <button onClick={() => setColor('red')}>Красный</button>
-            <button onClick={() => setColor('blue')}>Синий</button>
-            <button onClick={() => setColor('green')}>Зеленый</button>
+        <div className="color-picker">
+            <div 
+                className="color-picker__block"
+                style={{ backgroundColor: color }}
+            ></div>
+
+            <div className="color-picker__buttons">
+                <button 
+                    className="color-picker__button color-picker__button--red"
+                    onClick={() => setColor('red')}
+                >
+                    Красный
+                </button>
+
+                <button 
+                    className="color-picker__button color-picker__button--blue"
+                    onClick={() => setColor('blue')}
+                >
+                    Синий
+                </button>
+
+                <button 
+                    className="color-picker__button color-picker__button--green"
+                    onClick={() => setColor('green')}
+                >
+                    Зеленый
+                </button>
+            </div>
         </div>
-
-
     )
 }
